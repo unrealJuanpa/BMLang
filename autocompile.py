0ffb1e6d8d99dd93bc924d6327d3a38590d0081e
unrealJuanpa <juanpacloud@gmail.com>
2022-10-23 22:45:35 -0600

    ultimos cambios de autocompile

--- autocompile.py
+++ autocompile.py
@@ -2,9 +2,9 @@ import os
 
 name = ''.join([x for x in os.listdir() if x.endswith('.y')]).split('.')[0]
 
-#os.system('clear')
+os.system('clear')
 os.system(f'bison -d {name}.y')
 os.system(f'flex {name}.l')
 os.system(f'gcc lex.yy.c {name}.tab.c -lfl -o {name}')
-os.system(f'./{name}')
+#os.system(f'./{name}')
 os.system(f'./{name} < prueba.ml')
